f5153985c54ea00342e12a3ca674b8f3676b7c62
cdujeu <charles@ajaxplorer.info>
2016-04-25 15:10:05 +0200

    Add tests to check public links after moving a shared file around or renaming one of its parent folders.

--- tests/test_sharing.py
+++ tests/test_sharing.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 #
 # Copyright 2007-2014 Charles du Jeu - Abstrium SAS <team (at) pyd.io>
 #  This file is part of Pydio.
@@ -46,13 +47,23 @@ def detect_shared_link(webdriver, url, expect_working=True, preview=True, downlo
     assert "Pydio" in webdriver.title
 
     if expect_working:
-        preview_block_test = element_present(webdriver, id='mainImage', test_attribute='src')
+        test_att = 'src' if preview else ''
+        preview_block_test = element_present(webdriver, id='mainImage', test_attribute=test_att)
         assert (preview and preview_block_test) or (not preview and not preview_block_test)
         download_block_test = element_present(webdriver, id='download_button')
         assert (download and download_block_test) or (not download and not download_block_test)
     else:
         error_block_test = element_present(webdriver, css='div.hash_load_error')
         assert error_block_test
+
+
+def check_shared_element_data(sdk, filepath):
+    s_data = sdk.check_share_link(filepath)
+    data = json.loads(s_data)
+    if 'links' in data:
+        for key in data['links']:
+            return data['links'][key]['public_link']
+    return False
 
 
 def local_stat(path):
@@ -88,4 +99,38 @@ def test_shared_link(server_def, workspace, webdriver, preview, download):
         detect_shared_link(webdriver, link, expect_working=False)
     sdk.delete('/image.png')
 
-#def test_share_move(server_def, workspace, webdriver):
+def test_share_move(server_def, workspace, webdriver):
+    sdk = PydioSdk(server_def['host'], workspace['id'], unicode(''), '', (server_def['user'], server_def['pass']))
+
+    # Upload and create link
+    sdk.bulk_mkdir([u'/folder', u'/répertoire', u'/répertoire/content', u'/répertoire/content/link'])
+    sdk.upload(local='resources/image.png', local_stat=local_stat('resources/image.png'), path=unicode('/folder/image.png'))
+    link = sdk.share(ws_label='Shared File', ws_description='Description', password='', expiration='', downloads='',
+                     can_read='true', can_download='true', paths=u'/folder/image.png', link_handler='', can_write='false')
+    detect_shared_link(webdriver, link, expect_working=True, preview=True, download=True)
+    loaded_link = check_shared_element_data(sdk, u'/folder/image.png')
+    assert loaded_link == link
+
+    # Now move original and check link is still ok
+    sdk.rename('/folder/image.png', '/folder/image2.png')
+    detect_shared_link(webdriver, link, expect_working=True, preview=True, download=True)
+    loaded_link = check_shared_element_data(sdk, u'/folder/image2.png')
+    assert loaded_link == link
+
+    # Move to another folder
+    sdk.rename('/folder/image2.png', u'/répertoire/content/link/image2.png')
+    detect_shared_link(webdriver, link, expect_working=True, preview=True, download=True)
+    loaded_link = check_shared_element_data(sdk, u'/répertoire/content/link/image2.png')
+    assert loaded_link == link
+
+    # Rename a parent folder
+    sdk.rename(u'/répertoire/content', u'/répertoire/contenu renommé')
+    detect_shared_link(webdriver, link, expect_working=True, preview=True, download=True)
+    loaded_link = check_shared_element_data(sdk, u'/répertoire/contenu renommé/link/image2.png')
+    assert loaded_link == link
+
+    # Unshare. At first pass, detect correctly unshared.
+    sdk.unshare(u'/répertoire/contenu renommé/link/image2.png')
+    sdk.delete(u'/répertoire/contenu renommé/link/image2.png')
+    sdk.delete(u'/répertoire')
+    sdk.delete(u'/folder')
